1d2eee18970a6dd987fdcdc0474e73249f11213d
belokury <belokury@ya.ru>
2023-09-20 00:50:13 +0300

    Арифметические операции

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +text.length() );
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondtNumber  = new Scanner(System.in).nextInt();
+        System.out.println("Сумма: " + (firstNumber + secondtNumber ));
+        System.out.println("Разность: " + (firstNumber - secondtNumber ));
+        System.out.println("Произведение: " + (firstNumber * secondtNumber ));
+        System.out.println("Частное: " + ((double) firstNumber / secondtNumber ));
     }
 }
