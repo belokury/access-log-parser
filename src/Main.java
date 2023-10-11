bd0baec038ff897400c4152c647baf847af20894
sobaka <belokury@ua.ru>
2023-10-12 00:44:52 +0300

    Проверка пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
-import java.sql.SQLOutput;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int res = 0;
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +text.length() );
+        System.out.println("Длина текста: " + text.length());
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) {
+                System.out.println("Данный путь является путем к папке");
+                continue;
+            } else if (fileExists) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл № " + ++res);
+                continue;
+            }
+            System.out.println("Данный путь не существует");
+        }
     }
 }
